bbbb9113f6028b46ad7eb323758ae342d1651df1
Yaroslav <6715414@gmail.com>
2024-02-09 17:47:04 +0200

    refactor: delete redux from project

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/app/App';
 import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
-import { StoreProvider } from '@/app/providers/StoreProvider';
 
 import '@/app/styles/index.scss';
 
@@ -15,10 +14,8 @@ if (!container) {
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <StoreProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </StoreProvider>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>
 );
