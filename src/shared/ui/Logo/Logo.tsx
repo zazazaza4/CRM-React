e63873fb5d2b5152ef616cc8d084ff7c12b63e8a
Yaroslav <6715414@gmail.com>
2024-02-09 18:00:00 +0200

    refactor: improve Logo component to include additional functionality

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -7,15 +7,28 @@ import LogoIcon from '@/shared/assets/icons/logo.svg';
 
 import { AppLink } from '../AppLink';
 import { Icon } from '../Icon';
+import { Text } from '../Text';
 
 import cls from './Logo.module.scss';
 
 interface LogoProps {
   className?: string;
+  isCollapsed?: boolean;
 }
 
 export const Logo: FC<LogoProps> = memo((props: LogoProps) => {
-  const { className } = props;
+  const { className, isCollapsed } = props;
+
+  if (isCollapsed) {
+    return (
+      <AppLink
+        to={getRouteMain()}
+        className={classNames(cls.Logo, {}, [className])}
+      >
+        <Icon Svg={LogoIcon} />
+      </AppLink>
+    );
+  }
 
   return (
     <AppLink
@@ -23,6 +36,8 @@ export const Logo: FC<LogoProps> = memo((props: LogoProps) => {
       className={classNames(cls.Logo, {}, [className])}
     >
       <Icon Svg={LogoIcon} />
+      <Text title="Dashboard" weight="semi-bold" size="size_xl" theme="black" />
+      <Text className={cls.version} text="v.01" size="size_xs" />
     </AppLink>
   );
 });
