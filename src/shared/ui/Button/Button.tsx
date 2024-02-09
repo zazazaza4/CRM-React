5b792bbba627586170bc00fad4741bd4ddc0d2ce
Yaroslav <6715414@gmail.com>
2024-02-09 18:36:07 +0200

    refactor:  Button component with simplified prop types and unified theme options.

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,25 +4,11 @@ import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 
 import cls from './Button.module.scss';
 
-export enum ButtonTheme {
-  CLEAR = 'clear',
-  CLEAR_INVERTED = 'clearInverted',
-  OUTLINE = 'outline',
-  OUTLINE_RED = 'outlineRed',
-  BACKGROUND = 'background',
-  BACKGROUND_INVERTED = 'backgroundInverted',
-}
-
-export enum ButtonSize {
-  M = 'size_m',
-  L = 'size_l',
-  XL = 'size_xl',
-}
+type ButtonTheme = 'primary';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ButtonTheme;
-  size?: ButtonSize;
   isSquare?: boolean;
   isDisabled?: boolean;
   children?: ReactNode;
@@ -32,10 +18,9 @@ export const Button: FC<ButtonProps> = memo(
   ({
     className,
     children,
-    theme = ButtonTheme.OUTLINE,
+    theme = 'primary',
     isSquare,
     isDisabled,
-    size = ButtonSize.M,
     ...otherProps
   }: ButtonProps) => {
     const mods: Mods = {
@@ -46,11 +31,7 @@ export const Button: FC<ButtonProps> = memo(
     return (
       <button
         type="button"
-        className={classNames(cls.Button, mods, [
-          className,
-          cls[theme],
-          cls[size],
-        ])}
+        className={classNames(cls.Button, mods, [className, cls[theme]])}
         disabled={isDisabled}
         {...otherProps}
       >
