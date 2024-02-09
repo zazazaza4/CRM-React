96617e805c1f1680776e2a0e29a156eeff11c874
Yaroslav <6715414@gmail.com>
2024-02-09 19:00:53 +0200

    refactor: update MainPage component to include PageHeader and CustomerBlock components

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,17 @@
 import { Page } from '@/widgets/Page';
 
+import { CustomerBlock } from './CustomerBlock/CustomerBlock';
+import { PageHeader } from './PageHeader/PageHeader';
+
+import cls from './MainPage.module.scss';
+
 const MainPage = () => {
-  return <Page data-testid="MainPage">Main</Page>;
+  return (
+    <Page data-testid="MainPage">
+      <PageHeader className={cls.pageHeader} />
+      <CustomerBlock />
+    </Page>
+  );
 };
+
 export default MainPage;
