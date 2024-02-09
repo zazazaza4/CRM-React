92fc6ce7c8b9203abf3d0418d1738a290f2fea76
Yaroslav <6715414@gmail.com>
2024-02-09 19:02:12 +0200

    refactor: simplify componentRender function by removing StoreProvider and initialState parameter.

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -2,22 +2,17 @@ import { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
-import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
-
 export interface ComponentRenderOptions {
   route?: string;
-  initialState?: DeepPartial<StateSchema>;
 }
 
 export const componentRender = (
   component: ReactNode,
   options: ComponentRenderOptions = {}
 ) => {
-  const { route = '/', initialState } = options;
+  const { route = '/' } = options;
 
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <StoreProvider initialState={initialState}>{component}</StoreProvider>
-    </MemoryRouter>
+    <MemoryRouter initialEntries={[route]}>{component}</MemoryRouter>
   );
 };
